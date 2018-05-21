2369fa3baa082686daff513fc3d163235005293c
John Kelleher <johnkelleher@gmail.com>
2018-05-21 22:23:52 +1000

    Adds isValidChain method

--- blockchain.js
+++ blockchain.js
@@ -11,7 +11,22 @@ class Blockchain {
 
         return block;
     }
-}
 
+    isValidChain(chain) {
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
+        for (let i=1; i<chain.length; i++) {
+            const block = chain[i];
+            const lasBlock = chain[i-1];
+
+            if (block.lastHash !== lasBlock.hash ||
+                block.hash !== Block.blockHash(block)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
 
 module.exports = Blockchain;
