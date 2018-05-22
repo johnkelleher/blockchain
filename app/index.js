b7294e41daf44b241bdaaa755a4242e0b2c5aa8d
John Kelleher <johnkelleher@gmail.com>
2018-05-22 19:47:34 +1000

    Adds P2P Server

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
-
+const P2pServer = require('./p2p-server.js');
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -19,4 +20,7 @@ app.post('/mine', (req, res) => {
 
    res.redirect('/blocks');
 });
+
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+
+p2pServer.listen();
